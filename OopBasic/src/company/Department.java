51adba4ae633497f17b4607151ce5f6e36f69968
dir-FY24小林廉太郎 <rentarou.kobayashi.j411@dirbato.co.jp>
2024-04-09 09:13:38 +0900

    Lesson4終了段階

---
+++ OopBasic/src/company/Department.java
@@ -0,0 +1,35 @@
+package company;
+
+public class Department {
+    private final String name;
+    private final String departmentID;
+    private int budget;
+    
+    //コンストラクター
+    public Department(String name, String departmentID,int budget) {
+        this.name=name;
+        this.departmentID=departmentID;
+        this.budget = budget;
+    }
+        
+        //会議を開催
+        public void meeting() {
+            System.out.println("部内会議を開催します。　部署："+name+"/予算："+budget);
+            
+        }
+        public String getName() {
+            return name;
+        }
+        public double getBudget() {
+            return budget;
+        }
+        public void setBudget(int budget) {
+            if(budget<0) {
+                throw new IllegalArgumentException("予算はマイナスにできません");
+                
+            }
+            this.budget = budget;
+        
+    }
+
+}
---
+++ OopBasic/src/company/Employee.java
@@ -0,0 +1,36 @@
+package company;
+
+public abstract class Employee implements Workable {
+    // フィールド
+    protected final String name;
+    protected final Department department;
+    private final String position;
+    private final int employeeId;
+
+    // コンストラクター
+    public Employee(String name, Department department, String position, int employeeId) {
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.employeeId = employeeId;
+    }
+
+    // 報告メソッド
+    public void report(int times) {
+        System.out.println(times + "回目の報告をします。役職：" + position + "/名前：" + name);
+    }
+
+    // 報告メソッド（オーバーロード）
+    public void report() {
+        report(1);
+    }
+    
+    // 会議に参加するメソッド
+    public  abstract void joinMeeting() ;
+    
+    //働くメソッドを実装
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。 名前:"+ name+slogan);
+    }
+}
